ba293552b0496089262f2e5c95241ddd2a3eb031
Chris <christina@dreamcore.net>
2024-08-09 23:11:32 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,27 @@
+let quotes = [
+    { "date": "2024-08-08", "quote": "This is an example quote." },
+    { "date": "2024-08-07", "quote": "Another insightful thought." }
+];
+
+let currentQuoteIndex = 0;
+
+function enterSite() {
+    document.getElementById('landing').classList.add('hidden');
+    document.getElementById('content').classList.remove('hidden');
+    displayQuote();
+}
+
+function displayQuote() {
+    let quoteText = document.getElementById('quoteText');
+    let quoteDate = document.getElementById('quoteDate');
+
+    quoteText.innerHTML = `"${quotes[currentQuoteIndex].quote}"`;
+    quoteDate.innerHTML = `${quotes[currentQuoteIndex].date}`;
+
+    setTimeout(() => {
+        currentQuoteIndex = (currentQuoteIndex + 1) % quotes.length;
+        quoteText.classList.remove('fadeIn');
+        quoteDate.classList.remove('fadeIn');
+        displayQuote();
+    }, 5000);
+}
